d446a856d902bd31cd60b1605bd8818b1eac9447
Daria <dar.zavaliuk@gmail.com>
2023-02-22 02:16:26 +0300

    fix: change Cat class

--- Animals/src/ootpisp/Cat.java
+++ Animals/src/ootpisp/Cat.java
@@ -3,10 +3,10 @@ package ootpisp;
 import java.util.ArrayList;
 
 public class Cat extends DomesticAnimal {
-    private static int NUMS_LEGS_CAT = 4;
+    private static int NUM_LEGS_CAT = 4;
 
     public Cat(String name, int age) {
-        super(name, age, NUMS_LEGS_CAT);
+        super(name, age, NUM_LEGS_CAT);
     }
 
     public ArrayList<CatToys> toys = new ArrayList<>();
